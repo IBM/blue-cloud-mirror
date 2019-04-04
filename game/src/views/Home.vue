057c6d40075f40041b71d6733c5cf4eda5a25e7e
thomas-suedbroecker <thomas.suedbroecker@de.ibm.com>
2019-04-04 13:40:54 +0200

    update home and register

--- game/src/views/Home.vue
+++ game/src/views/Home.vue
@@ -3,15 +3,10 @@
     <h1 style="margin-top:15px;margin-bottom:25px"><p>Welcome to our <b>#BlueCloudMirror</b> game</p></h1>
 
     <div style="margin-top:15px;">
-      You need to show five <b>emotions</b> and five <b>poses</b> in <b>two levels</b>. 
-      The <b>fastest player wins</b>.
+      Show five <b>emotions</b> and five <b>poses</b> in <b>two levels</b>. 
+      The <b>faster, the better</b>.
     </div>
 
-    <div style="margin-top:15px;">
-      <p>The game is designed to be a conversation starter at conferences for developers. Having fun, being interactive and using key technologies of the IBM Cloud at the same time. Developers can play anonymously or they can register themselvs and accept the terms and conditions to get the chance to win gadgets at the end of conferences.</p>
-    </div>
-
-    <div style="margin-top:10px;"></div>
     <div style="margin-top:10px;">
     <center><b-button 
        block 
@@ -22,93 +17,98 @@
     </div>
 
     <div style="margin-top:35px;"></div>
-    <h2 style="margin-top:15px;margin-bottom:25px"><b>#BlueCloudMirror</b> is based on three major technologies and architecture parts</h2>
+    <h2 style="margin-top:15px;margin-bottom:25px">Explore the architecture of #BlueCloudMirror</h2>
 
     <b-container class="major-technologies">
-      <b-row>
-        <b-col>
-          <b-card>
-            <b-media>
-              <b-img slot="aside" v-bind="iconProps" alt="ibm-cloud-functions-icon" wid
-              rounded="circle"
-              src="ibm-cloud-functions-icon.png"></b-img>
-              <h3 class="mt-serverless">Serverless</h3>             
-                <li>automated scale up and to zero</li>
-                <li>event driven code</li>
-                <li>stateless compute</li>
-                <p>... and a lot more.</p>
-            </b-media>
-          </b-card>
-        </b-col>
-        <b-col>
-          <b-link :to="'architecture'"><font color=black>
-           <b-card>       
-            <b-media right-align vertical-align="center">
-              <b-img slot="aside" rounded v-bind="diagramProps" alt="game-serverless" 
-              src="game-serverless.png"></b-img>
-              <h3 class="mt-game">Game</h3>
-              The game uses Serverless for more information click the architecture image.
-            </b-media>       
-           </b-card>
-          </font></b-link>
-        </b-col>
+      <hr>
+      <b-row style="border-style:solid; border-width:0px;">
+          <table style="width:100%">
+          <td style="width:10%; text-align:center; vertical-align:middle"> 
+                <b-link :to="'architecture'"><b-img  v-bind="iconProps" alt="ibm-cloud-functions-icon" wid
+                rounded="circle"
+                src="ibm-cloud-functions-icon.png"></b-img></b-link>
+          </td>
+          <td style="width:25%; vertical-align:top">
+                <h5 class="mt-serverless">Serverless</h5>             
+                  <li>automated scale up and to zero</li>
+                  <li>event driven code</li>
+                  <li>stateless compute</li>
+          </td>
+          <td style="width:35%; vertical-align:top">    
+                  <h5 class="mt-game">Core game</h5>
+                  Implemented as serverless web application,
+                  <br> since ...
+                  <li>it is not accessed 24x7</li>
+                  <li>to make developers productive</li>
+          </td>
+          <td style="width:35%; vertical-align:top">
+                  <h5 class="mt-game">Technologies</h5>
+                  <li>Vue.js</li>
+                  <li>TensorFlow.js</li>
+                  <li>IBM Cloud Functions based on Apache OpenWhisk</li>
+          </td>
+          </table>
       </b-row>
-      <b-row>
-        <b-col>
-          <b-card>
-            <b-media>
-              <b-img slot="aside" v-bind="iconProps" alt="kubernetes-icon" 
-              rounded="circle"
-              src="kubernetes-icon.png"></b-img>
-              <h3 class="mt-kubernetes">Kubernetes</h3>
-              <p>
-                <li>container platform</li>
-                <li>microservices platform</li>
-                <li>portable cloud platform</li> 
-                <p>... and a lot more.</p>
-            </b-media>
-          </b-card>
-        </b-col>
-        <b-col>
-          <b-link :to="'architecture'"><font color=black>
-           <b-card>
-            <b-media right-align vertical-align="center">
-              <b-img slot="aside" rounded v-bind="diagramProps" left alt="users-kubernetes" 
-              src="users-kubernetes.png"></b-img>
-              <h3 class="mt-users">Users</h3>
-              The Users uses Kubernetes for more information click the architecture image.
-            </b-media>
-           </b-card>
-          </font></b-link>
-        </b-col>
+      <hr>
+      <b-row style="border-style:solid; border-width:0px;">
+          <table style="width:100%">
+          <td style="width:10%; text-align:center; vertical-align:middle">  
+                <b-link :to="'architecture'"><b-img 
+                v-bind="iconProps" alt="kubernetes-icon" 
+                rounded="circle"
+                src="kubernetes-icon.png"></b-img></b-link>
+          </td>
+          <td style="width:25%; vertical-align:top">
+              <h5 class="mt-kubernetes">Kubernetes</h5>
+              <li>container orchestration framework</li>
+              <li>production-grade open source project</li>
+              <li>portable workloads</li> 
+          </td>
+          <td style="width:35%; vertical-align:top">    
+                  <h5 class="mt-game">Users service</h5>
+                  Implemented via on-premises Kubernetes environments to ...
+                  <li>avoid storing personal data in a public cloud</li>
+                  <li>allow the service to be moved easily on other environments</li>
+          </td>
+          <td style="width:35%; vertical-align:top">
+                  <h5 class="mt-game">Technologies</h5>
+                  <li>Vue.js</li>
+                  <li>TensorFlow.js</li>
+                  <li>IBM Cloud Functions based on Apache OpenWhisk</li>
+          </td>
+          </table>
       </b-row>
-      <b-row>
-        <b-col>
-          <b-card>
-            <b-media>
-              <b-img slot="aside" v-bind="iconProps" alt="kubernetes-icon" 
-              rounded="circle" src="cloud-foundry-icon.png"></b-img>
-              <h3 class="mt-cloud-foundry">Cloud Foundry</h3>
-                <li>developer productivity first</li> 
-                <li>easy build and deploy</li>
-                <li>easy integration of services</li>
-                <p>... and a lot more.</p>
-            </b-media>
-          </b-card>
-        </b-col>
-        <b-col>
-          <b-link><font color=black>
-           <b-card>
-            <b-media right-align vertical-align="center">
-              <b-img slot="aside" rounded v-bind="diagramProps" left alt="scores-cloud-foundry" 
-              src="scores-cloud-foundry.png"></b-img>
-              <h3 class="mt-scores">Scores</h3>
-              The Scores uses Cloud Foundry for more information click the architecture image.
-            </b-media>
-          </b-card>
-         </font></b-link>
-        </b-col>
+      <hr>
+      <b-row style="border-style:solid; border-width:0px;">
+          <table style="width:100%">
+          <td style="width:10%; text-align:center; vertical-align:middle">  
+                <b-link :to="'architecture'"><b-img 
+                v-bind="iconProps" alt="cloud-foundry-icon" 
+                rounded="circle"
+                src="cloud-foundry-icon.png"></b-img></b-link>
+          </td>
+          <td style="width:25%; vertical-align:top">
+              <h5 class="mt-kubernetes">Cloud Foundry</h5>
+              <li>speed and simplify development</li> 
+              <li>open source project for public and private clouds</li>
+              <li>easy integration of services</li>
+          </td>
+          <td style="width:35%; vertical-align:top">    
+                  <h5 class="mt-game">Users service</h5>
+                  Scores service and web application <br>
+                  Implemented via Cloud Foundry to ...
+                  <li>allow quick theme updates to the web application</li>
+                  <li>allow developers to focus on code rather than infrastructure </li>
+          </td>
+          <td style="width:35%; vertical-align:top">
+                  <h5 class="mt-game">Technologies</h5>
+                  <li>IBM's Cloud Foundry Enterprise Environment</li>
+                  <li>Vue.js</li>
+                  <li>IBM Cloudant (NoSQL database)</li>
+          </td>
+          </table>
       </b-row>
+      <hr>
     </b-container>
     </div>
 </template>
